0966f2f7a542b6b18908bbb0cda97c99ad99ad6c
jondujaka <24923320+jondujaka@users.noreply.github.com>
2025-04-27 16:41:24 +0200

    fix fonts

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -18,7 +18,7 @@ export default async function handleRequest(
     },
     mediaSrc: ["https://checkout.null-space.eu"],
     styleSrc: ["https://fonts.googleapis.com"],
-    fontSrc: ["https://fonts.gstatic.com", "http://localhost:3000", "https://*.myshopify.dev"]
+    fontSrc: ["https://fonts.gstatic.com", "http://localhost:3000", "https://cdn.shopify.com/"]
   });
 
   const body = await renderToReadableStream(
