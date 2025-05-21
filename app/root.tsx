2dc295f3551dd4b25bfcb7200fd073c3de1ee61b
jondujaka <24923320+jondujaka@users.noreply.github.com>
2025-05-21 17:10:51 +0200

    test klaviyo

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import {Analytics, getShopAnalytics, useNonce} from '@shopify/hydrogen';
-import {type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import { Analytics, getShopAnalytics, Script, useNonce } from '@shopify/hydrogen';
+import { type LoaderFunctionArgs } from '@shopify/remix-oxygen';
 import {
   Outlet,
   useRouteError,
@@ -12,10 +12,10 @@ import {
   useRouteLoaderData,
 } from '@remix-run/react';
 import favicon from '~/assets/favicon.svg';
-import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
+import { FOOTER_QUERY, HEADER_QUERY } from '~/lib/fragments';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
-import {PageLayout} from './components/PageLayout';
+import { PageLayout } from './components/PageLayout';
 
 export type RootLoader = typeof loader;
 
@@ -61,7 +61,7 @@ export function links() {
       rel: 'preconnect',
       href: 'https://shop.app',
     },
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+    { rel: 'icon', type: 'image/svg+xml', href: favicon },
   ];
 }
 
@@ -72,7 +72,7 @@ export async function loader(args: LoaderFunctionArgs) {
   // Await the critical data required to render initial state of the page
   const criticalData = await loadCriticalData(args);
 
-  const {storefront, env} = args.context;
+  const { storefront, env } = args.context;
 
   return {
     ...deferredData,
@@ -97,8 +97,8 @@ export async function loader(args: LoaderFunctionArgs) {
  * Load data necessary for rendering content above the fold. This is the critical data
  * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
  */
-async function loadCriticalData({context}: LoaderFunctionArgs) {
-  const {storefront} = context;
+async function loadCriticalData({ context }: LoaderFunctionArgs) {
+  const { storefront } = context;
 
   const [header] = await Promise.all([
     storefront.query(HEADER_QUERY, {
@@ -110,7 +110,7 @@ async function loadCriticalData({context}: LoaderFunctionArgs) {
     // Add other queries here, so that they are loaded in parallel
   ]);
 
-  return {header};
+  return { header };
 }
 
 /**
@@ -118,8 +118,8 @@ async function loadCriticalData({context}: LoaderFunctionArgs) {
  * fetched after the initial page load. If it's unavailable, the page should still 200.
  * Make sure to not throw any errors here, as it will cause the page to 500.
  */
-function loadDeferredData({context}: LoaderFunctionArgs) {
-  const {storefront, customerAccount, cart} = context;
+function loadDeferredData({ context }: LoaderFunctionArgs) {
+  const { storefront, customerAccount, cart } = context;
 
   // defer the footer query (below the fold)
   const footer = storefront
@@ -141,7 +141,7 @@ function loadDeferredData({context}: LoaderFunctionArgs) {
   };
 }
 
-export function Layout({children}: {children?: React.ReactNode}) {
+export function Layout({ children }: { children?: React.ReactNode }) {
   const nonce = useNonce();
   const data = useRouteLoaderData<RootLoader>('root');
 
@@ -167,6 +167,8 @@ export function Layout({children}: {children?: React.ReactNode}) {
         ) : (
           children
         )}
+        <Script async type="text/javascript"
+          src="//static.klaviyo.com/onsite/js/ULWjSM/klaviyo.js" />
         <ScrollRestoration nonce={nonce} />
         <Scripts nonce={nonce} />
       </body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import {  type MetaFunction } from '@remix-run/react';
 import ComingSoon from '~/components/ComingSoon';
+import { Script } from '@shopify/hydrogen';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'NullSpace' }];
@@ -8,5 +9,5 @@ export const meta: MetaFunction = () => {
 
 export default function Homepage() {
  
-  return <ComingSoon/>
+  return <div><ComingSoon/><div className="newsletter klaviyo-form-Uf3hBV"/></div>
 }
