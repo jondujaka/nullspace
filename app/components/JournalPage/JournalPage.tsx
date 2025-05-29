566410d9309046f4a44ef55ee28a2eee5b510674
jondujaka <24923320+jondujaka@users.noreply.github.com>
2025-05-29 22:24:03 +0200

    journal page

--- app/components/JournalPage/JournalPage.tsx
+++ app/components/JournalPage/JournalPage.tsx
@@ -6,39 +6,14 @@ export default function JournalPage({ media }: { media: MediaImage[] }) {
     return (
         <div className={styles.wrapper}>
 
-            <div>
-                {media[0].image && <Image
-                    width={media[0].image.width ?? "auto"}
-                    height={media[0].image.height ?? "auto"}
-                    data={media[0].image}
-                />}
-            </div>
 
+            {media.map(media => media.image && <div key={media.id}><Image
 
-            <div className={styles.row}>
-                {media[1].image && media[2].image &&
-                    <>
-                        <Image
-                            width={media[1].image.width ?? "auto"}
-                            height={media[1].image.height ?? "auto"}
-                            data={media[1].image}
-                        />
-                        <Image
-                            width={media[2].image.width ?? "auto"}
-                            height={media[2].image.height ?? "auto"}
-                            data={media[2].image}
-                        />
-                    </>
-                }
-            </div>
+                data={media.image}
+                sizes="(min-width:1600px) 1600px, (min-width: 960px) 70vw, 100vw"
+                loading='lazy'
+            /></div>)}
 
-            <div>
-                {media[3].image && <Image
-                    width={media[3].image.width ?? "auto"}
-                    height={media[3].image.height ?? "auto"}
-                    data={media[3].image}
-                />}
-            </div>
         </div>
     )
 }
