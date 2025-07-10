67bac7df4f142309359d74d8a34617dbfd91f2c0
jondujaka <24923320+jondujaka@users.noreply.github.com>
2025-07-10 18:21:00 +0200

    test gtag

--- app/root.tsx
+++ app/root.tsx
@@ -203,15 +203,28 @@ export function Layout({children}: {children?: React.ReactNode}) {
         />
         <Script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ET02ZCGZ4T');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNDTGM88');`,
           }}
         />
       </head>
       <body>
+        {/*  Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            title="gtag"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNDTGM88"
+            height="0"
+            width="0"
+            style={{display: 'none', visibility: 'hidden'}}
+          ></iframe>
+        </noscript>
+
+        {/* End Google Tag Manager (noscript) */}
+
         {data ? (
           <Analytics.Provider
             cart={data.cart}
